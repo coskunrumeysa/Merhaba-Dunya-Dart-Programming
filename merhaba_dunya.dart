1da7504bb961e90aa9146761eb1d8345ef1fe7d3
coskunrumeysa <70705198+coskunrumeysa@users.noreply.github.com>
2025-02-14 16:22:29 +0300

    Add files via upload

---
+++ merhaba_dunya.dart
@@ -0,0 +1,12 @@
+void main() {
+  //Dosya isimlendirilmesi yaparken snake_case kullanılır : merhaba_dunya
+  //Değişken isimlendirlirken camelCase kullanılır : merhabaDunya
+  //Sınıf isimlendirilirken PascalCase kullanılır : MerhabaDunya
+
+  print('Merhaba Dünya');
+
+  //Yazılım kuralı olarak tırnak kullanmak için \' şeklide bir kullanım gerekmeketdir.
+  print('Ben Dart \'ı öğreneceğim.');
+
+  //Program F5 'e basıldığında açılan launch.json dosyasında "program":"${file}" şeklinde eklenmelidir.
+}
